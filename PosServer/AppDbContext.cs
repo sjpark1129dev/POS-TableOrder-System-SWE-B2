d4fb52bffa91fa9dab86d2148f5fa26a03ebea87
sjPark <sjtemp62@gmail.com>
2025-05-19 08:50:48 +0900

    DBConnection 추가

---
+++ PosServer/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PosServer
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
