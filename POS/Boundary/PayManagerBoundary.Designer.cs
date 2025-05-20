f506743883fd1fbb49ec700fe0e2272e92bd2f56
jeonchaemyeong <jeonchaemyeong@naver.com>
2025-05-20 23:09:51 +0900

    paymangaer

---
+++ POS/Boundary/PayManagerBoundary.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace POS.Boundary
+{
+    partial class PayManagerBoundary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            Total = new Label();
+            cancelAll = new Button();
+            cancelselect = new Button();
+            EditOrder = new Button();
+            cardPayment = new Button();
+            cashPayment = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(147, 72);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(465, 150);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // Total
+            // 
+            Total.AutoSize = true;
+            Total.Location = new Point(472, 225);
+            Total.Name = "Total";
+            Total.Size = new Size(42, 15);
+            Total.TabIndex = 1;
+            Total.Text = "총액 : ";
+            Total.Click += label1_Click;
+            // 
+            // cancelAll
+            // 
+            cancelAll.Location = new Point(147, 253);
+            cancelAll.Name = "cancelAll";
+            cancelAll.Size = new Size(130, 26);
+            cancelAll.TabIndex = 2;
+            cancelAll.Text = "전체 취소";
+            cancelAll.UseVisualStyleBackColor = true;
+            // 
+            // cancelselect
+            // 
+            cancelselect.Location = new Point(312, 253);
+            cancelselect.Name = "cancelselect";
+            cancelselect.Size = new Size(130, 26);
+            cancelselect.TabIndex = 3;
+            cancelselect.Text = "선택 취소";
+            cancelselect.UseVisualStyleBackColor = true;
+            // 
+            // EditOrder
+            // 
+            EditOrder.Location = new Point(482, 253);
+            EditOrder.Name = "EditOrder";
+            EditOrder.Size = new Size(130, 26);
+            EditOrder.TabIndex = 4;
+            EditOrder.Text = "주문 수정";
+            EditOrder.UseVisualStyleBackColor = true;
+            // 
+            // cardPayment
+            // 
+            cardPayment.Location = new Point(147, 298);
+            cardPayment.Name = "cardPayment";
+            cardPayment.Size = new Size(130, 26);
+            cardPayment.TabIndex = 5;
+            cardPayment.Text = "카드 결제";
+            cardPayment.UseVisualStyleBackColor = true;
+            // 
+            // cashPayment
+            // 
+            cashPayment.Location = new Point(312, 298);
+            cashPayment.Name = "cashPayment";
+            cashPayment.Size = new Size(130, 26);
+            cashPayment.TabIndex = 6;
+            cashPayment.Text = "현금 결제";
+            cashPayment.UseVisualStyleBackColor = true;
+            // 
+            // PayManagerBoundary
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(696, 450);
+            Controls.Add(cashPayment);
+            Controls.Add(cardPayment);
+            Controls.Add(EditOrder);
+            Controls.Add(cancelselect);
+            Controls.Add(cancelAll);
+            Controls.Add(Total);
+            Controls.Add(listView1);
+            Name = "PayManagerBoundary";
+            Text = "Form1";
+            Load += PayManagerBoundary_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Label Total;
+        private Button cancelAll;
+        private Button cancelselect;
+        private Button EditOrder;
+        private Button cardPayment;
+        private Button cashPayment;
+    }
+}
---
+++ POS/Boundary/PayManagerBoundary.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS.Boundary
+{
+    public partial class PayManagerBoundary : Form
+    {
+        public PayManagerBoundary()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PayManagerBoundary_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+
+            // 컬럼 추가
+            listView1.Columns.Add("메뉴명", 150, HorizontalAlignment.Left);
+            listView1.Columns.Add("단가", 70, HorizontalAlignment.Right);
+            listView1.Columns.Add("수량", 50, HorizontalAlignment.Center);
+            listView1.Columns.Add("가격", 90, HorizontalAlignment.Right);
+
+            // 예시 데이터 추가
+            AddMenuItem("불고기", 12000, 2);
+            AddMenuItem("김치찌개", 8000, 1);
+
+            UpdateTotalPrice(); // 총액 초기 계산 및 표시
+        }
+
+        private void AddMenuItem(string name, int unitPrice, int quantity)
+        {
+            int totalPrice = unitPrice * quantity;
+
+            ListViewItem item = new ListViewItem(name);
+            item.SubItems.Add(unitPrice.ToString("N0"));
+            item.SubItems.Add(quantity.ToString());
+            item.SubItems.Add(totalPrice.ToString("N0"));
+
+            listView1.Items.Add(item);
+
+            UpdateTotalPrice(); // 아이템 추가 시마다 총액 갱신
+        }
+
+        private void UpdateTotalPrice()
+        {
+            int total = 0;
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                string priceText = item.SubItems[3].Text;
+
+                if (int.TryParse(priceText.Replace(",", ""), out int price))
+                {
+                    total += price;
+                }
+            }
+
+            Total.Text = "총액 : " + total.ToString("N0") + "원"; // 이렇게 바꾸면 됨
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
