33407f0e634d9955d62d65bc3c352da8314746c8
오현진 <hyunjin9126@gmail.com>
2025-05-27 22:58:25 +0900

    sales 코드 생성

---
+++ POS/Controller/SalesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POS.Entity;
+using POS.Repository;
+
+namespace POS.Controller
+{
+    public class SalesController
+    {
+        private SalesRepository salesRepository;
+
+        public List<SalesEntity> getAllSales()
+        {
+
+            return new List<SalesEntity>();
+        }
+
+        public List<SalesEntity> SearchSales(string menuName, DateTime startDate, DateTime endDate, string receiptNum)
+        {
+            return new List<SalesEntity>();
+        }
+    }
+}
---
+++ POS/Entity/SalesEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Entity
+{
+    public class SalesEntity
+    {
+        public DateTime salesDate;
+        public string recnum;
+        public int tableID;
+        public string menuName;
+        public int qty;
+        public int unitPrice;
+        public int price;
+    }
+}
