e5dbce20c7bf714348835443fd8a405fa13c780e
Choi Yeong in <cyi200258@gmail.com>
2025-05-27 21:59:30 +0900

    메뉴관리 컨트롤러 및 리포지토리 수정

---
+++ POS/Controller/MenuRemoveController.cs
@@ -0,0 +1,20 @@
+﻿using POS.Entity;
+using POS.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Controller
+{
+    public class MenuRemoveController
+    {
+        private MenuRepository menuRepository;
+
+        public void MenuRemove(MenuEntity menu)
+        {
+
+        }
+    }
+}
