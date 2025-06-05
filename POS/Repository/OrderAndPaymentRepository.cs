6365789218cfec5d5a5c18b17fda9022007bbfdd
sjtemp62 <sjtemp62@gmail.com>
2025-06-05 20:36:13 +0900

    결제관리 커밋

--- POS/Repository/OrderAndPaymentRepository.cs
+++ POS/Repository/OrderAndPaymentRepository.cs
@@ -1,45 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using POS.Domain;
+using System;
+using System.Linq;
 
 namespace POS.Repository
 {
     internal class OrderAndPaymentRepository
     {
-
         private readonly AppDbContext _context = AppDbContext.Instance;
 
-
-        public void insertPayment()
+        public bool ProcessPaymentByTable(int tableId)
         {
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                var unpaidOrders = _context.Orders
+                    .Include(o => o.Items)
+                    .Where(o => o.TableId == tableId && !o.IsPaid)
+                    .ToList();
+
+                if (!unpaidOrders.Any())
+                    return false;
+
+                string recNum = $"{DateTime.Now:yyyyMMdd}-{Guid.NewGuid().ToString()[..4]}";
+
+                foreach (var order in unpaidOrders)
+                {
+                    foreach (var item in order.Items)
+                    {
+                        var sale = new SalesEntity
+                        {
+                            SalesDate = DateTime.Now,
+                            RecNum = recNum,
+                            TableId = order.TableId,
+                            MenuName = item.MenuName,
+                            Qty = item.Qty,
+                            UnitPrice = item.UnitPrice,
+                            Price = item.Qty * item.UnitPrice
+                        };
+
+                        _context.Sales.Add(sale);
+                    }
+
+                    order.IsPaid = true;
+                }
+
+                _context.SaveChanges();
+                transaction.Commit();
+
+                return true;
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
         }
 
-        public void updatePayment()
+        public List<dynamic> GetUnpaidOrdersForGridByTable(int tableId)
         {
-        }
+            var result = _context.Orders
+                .Include(o => o.Items)
+                .Where(o => o.TableId == tableId && !o.IsPaid)  // ✅ 여기!
+                .SelectMany(order => order.Items.Select(item => new
+                {
+                    TableId = order.TableId,
+                    MenuName = item.MenuName,
+                    Qty = item.Qty,
+                    UnitPrice = item.UnitPrice,
+                    Total = item.Qty * item.UnitPrice,
+                    OrderTime = order.CreatedAt
+                }))
+                .ToList<dynamic>();
 
-        public void deletePayment()
-        {
-        }
-
-        public List<OrderEntity> LoadAllOrders()
-        {
-            return null;
-        }
-
-        public void updateOrder(OrderEntity order)
-        {
-        }
-
-        public void deleteOrder(OrderEntity order)
-        {
-        }
-
-        public void saveOrder(List<OrderEntity> orders)
-        {
+            return result;
         }
     }
 }
